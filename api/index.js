7dbfeb693a3fdda5adf37de8e5d4d52178c553c8
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-12 14:55:32 +0300

    rabichitу2

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/userRoutes');
 const mongoose = require('mongoose');  
 
 // Загрузка конфигурации  
-const token = process.env.TELEGRAM_BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';  
+const token = process.env.TELEGRAM_BOT_TOKEN || '7431411001:AAHx9_TODfc7VOlRfcXeab9bbiHeYgl-iNs';  
 const webAppUrl = 'https://strology.vercel.app/';  
 
 // Подключение к MongoDB  
