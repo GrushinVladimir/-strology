1e9c3c329512424c3e86ff6453325fa23199ad46
Aroniero7 <vasya301995@gmail.com>
2024-11-19 19:42:08 +0300

    изменения чата, добавление АПИ чатгпт

--- client/src/components/Body/ChatPage.jsx
+++ client/src/components/Body/ChatPage.jsx
@@ -42,7 +42,7 @@ function ChatPage() {
       const response = await axios.post(
         'https://api.openai.com/v1/chat/completions',
         {
-          model: 'gpt-4', // используйте правильную модель, например gpt-4
+          model: 'gpt-4o-mini', // используйте правильную модель, например gpt-4
           messages: [
             { role: 'user', content: finalMessage },
           ],
