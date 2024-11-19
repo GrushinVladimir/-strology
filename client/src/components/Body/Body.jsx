6dbaaa93fa50f18e168d5179a2dade45f01328cc
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-19 14:48:58 +0300

    goroskop

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -77,7 +77,7 @@ const Body = ({ step, userName, handleStart, handleNext, formData }) => {
     console.log('Данные для отправки:', userData); // Логируем данные  
 
     try {  
-        await axios.post('https://strology.vercel.app/api/users', userData);  
+        await axios.post('https://strology.vercel.app/pages/api/users', userData);  
         navigate('/main', { state: { zodiacSign: sign } });  
     } catch (error) {  
         console.error('Ошибка при сохранении данных пользователя:', error.response?.data || error.message);  
