80f5f01a86f7d16c600274430175cf809660b9c9
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-19 13:09:28 +0300

    ava

--- client/src/components/Body/ProfilePage.jsx
+++ client/src/components/Body/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 const ProfilePage = ({ telegramId }) => {
