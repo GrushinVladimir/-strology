bd9b61a59648865e54087e11f74c573c31149764
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-20 13:30:22 +0300

    profil

--- client/src/App.js
+++ client/src/App.js
@@ -77,7 +77,7 @@ function App() {
         <Route path="/main" element={<MainPage telegramId={telegramId} />} /> {/* Передаём telegramId как пропс */}
         <Route path="/profile" element={<ProfilePage telegramId={telegramId}/>} />
         <Route path="/test" element={<Test />} />
-        <Route path="/zadaniya" element={<Zadaniya />} />
+        <Route path="/zadaniya" element={<Zadaniya telegramId={telegramId}/>} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </div>
