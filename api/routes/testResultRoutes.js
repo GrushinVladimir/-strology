d1e8f55b555bde8fb968a5616629f8cdf16fbdac
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-20 12:42:58 +0300

    profil

--- api/routes/testResultRoutes.js
+++ api/routes/testResultRoutes.js
@@ -2,25 +2,25 @@ const express = require('express');
 const TestResult = require('../models/TestResult');  
 const router = express.Router();  
 
-
 router.post('/', async (req, res) => {  
-    const { telegramId, answers, dateCompleted } = req.body;  // Измените userId на telegramId  
-    console.log('Сохранение результатов для telegramId:', telegramId); // Лог для отладки  
-
+    const { userId, answers, dateCompleted } = req.body;  
+    console.log('Сохранение результатов для userId:', userId); 
     const newTestResult = new TestResult({  
-        userId: telegramId,  // Используйте telegramId вместо userId  
+        userId,  
         answers,  
         dateCompleted,  
     });  
   
     try {  
         await newTestResult.save();  
-        res.status(201).json({ message: 'Результаты теста успешно сохранены' });   
+        res.status(201).json({ message: 'Результаты теста успешно сохранены' }); 
+        
     } catch (error) {  
         console.error('Ошибка при сохранении результатов теста:', error);  
         res.status(500).json({ message: 'Ошибка сервера' });  
     }  
-});  
+  });  
+  
   module.exports = router;  
 
   router.get('/:telegramId', async (req, res) => {  
