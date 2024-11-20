d625397ebb9153997a92b7fefeb74b30cab327c3
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-20 12:01:56 +0300

    profil

--- api/routes/testResultRoutes.js
+++ api/routes/testResultRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const TestResult = require('../models/TestResult');  
 const router = express.Router();  
 
-
-
-
-
 router.post('/', async (req, res) => {  
     const { userId, answers, dateCompleted } = req.body;  
   
@@ -25,3 +21,13 @@ router.post('/', async (req, res) => {
   });  
   
   module.exports = router;  
+
+  router.get('/:telegramId', async (req, res) => {  
+    try {  
+        const results = await TestResult.find({ userId: req.params.userId });  
+        res.json(results);  
+    } catch (error) {  
+        console.error('Ошибка при получении результатов теста:', error);  
+        res.status(500).json({ message: 'Ошибка сервера' });  
+    }  
+});  
