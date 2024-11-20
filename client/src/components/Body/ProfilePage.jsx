dc1111b8eb2b619abc00452ad38e08835da2a946
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-20 12:50:52 +0300

    profil

--- client/src/components/Body/ProfilePage.jsx
+++ client/src/components/Body/ProfilePage.jsx
@@ -115,7 +115,17 @@ const ProfilePage = ({ telegramId }) => {
                           >Выполнено</button>   
             ) : (  
               <Link to="/test">  
-
+               <button className='button na'   
+                            style={{  
+                              position: 'absolute', margin: '2vh auto auto', display: 'block', left: 'unset', transform: 'none', padding: '6px 15px',  
+                              bottom: '0',  
+                              transform: 'translateX(-50%)',  
+                              left: '50%',  
+                              width: '115px',  
+                              fontSize: '15px'  
+                            }}  
+            
+                          >Летс гоу</button>   
             </Link>  
             )}  
           </div>  
