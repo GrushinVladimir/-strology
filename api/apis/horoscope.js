a257a4ba93f8b4782fcb4c1c021f975c579a708d
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-21 09:55:05 +0300

    gor

--- api/apis/horoscope.js
+++ api/apis/horoscope.js
@@ -44,7 +44,11 @@ async function handler(req, res) {
   }  
 
   try {  
-    const { data } = await axios.get(url);  
+    const { data } = await axios.get(url, {  
+      headers: {  
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'  
+      }  
+    });  
     console.log(data); // Логируем полученные данные  
     const $ = load(data);  
     const horoscopeText = $('div.horoscope__content p').text().trim();  
