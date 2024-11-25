536e43bbb6c36437db9a3db3713022f4ecac17a1
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-25 12:19:35 +0300

    Pay

--- client/src/components/Body/ProfilePage.jsx
+++ client/src/components/Body/ProfilePage.jsx
@@ -106,21 +106,23 @@ const ProfilePage = ({ telegramId }) => {
     const startPayment = async (telegramId) => {  
         const providerToken = 'sk_test_51QOxxP00guE2oMvt7aEjZrIPn44UB7TGa1c3d5SaLUho7se4fdfgOhPsLagsSee04srl2xdVOOt9vMstZ0QEdNLI003eMfDpVa'; // Замените на ваш токен  
         const invoiceData = {  
-            chat_id: telegramId,  
-            title: 'Оплата за услуги',  
-            description: 'Оплата за услуги',  
-            payload: `payload_${telegramId}`,  
-            provider_token: providerToken,  
-            start_parameter: 'test_payment',  
-            currency: 'RUB',  
-            prices: [{ label: 'Услуга', amount: 10000 }]  
+            chat_id: telegramId, // Идентификатор чата  
+            title: 'Оплата за услуги', // Название инвойса (обязательно)  
+            description: 'Оплата за услуги', // Описание инвойса (обязательно)  
+            payload: `payload_${telegramId}`, // Уникальный идентификатор для вашего платежа  
+            provider_token: providerToken, // Токен платежного провайдера (обязательно)  
+            start_parameter: 'test_payment', // Параметр для начала платежа  
+            currency: 'RUB', // Валюта  
+            prices: [{ label: 'Услуга', amount: 10000 }] // Сумма в копейках (100 руб.)  
         };  
     
         try {  
-            console.log('Invoice Data:', invoiceData);  
+            console.log('Invoice Data перед отправкой:', JSON.stringify(invoiceData, null, 2)); // Логирование для отладки  
             const response = await fetch(`https://api.telegram.org/bot7431411001:AAHx9_TODfc7VOlRfcXeab9bbiHeYgl-iNs/sendInvoice`, {  
                 method: 'POST',  
-                headers: { 'Content-Type': 'application/json' },  
+                headers: {  
+                    'Content-Type': 'application/json',  
+                },  
                 body: JSON.stringify(invoiceData),  
             });  
     
@@ -136,8 +138,7 @@ const ProfilePage = ({ telegramId }) => {
             console.error('Ошибка:', error);  
             alert(`Произошла ошибка: ${error.message}`);  
         }  
-    };   
-
+    };  
 
     return (  
         <div className='Prof'>  
