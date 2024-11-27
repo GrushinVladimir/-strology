9a052cf174013d70df625a918c2c1b5880bacb05
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-27 13:48:08 +0300

    active

--- client/src/components/Body/ProfilePage.jsx
+++ client/src/components/Body/ProfilePage.jsx
@@ -261,7 +261,7 @@ const ProfilePage = ({ telegramId }) => {
                 </Link>  
                 <Link to="/profile">  
                     <img src="img/menu/profile.png" style={{ width: '13px' }} alt="Профиль" />  
-                    <span style={{color:'white'}}>Профиль</span>  
+                    <span style={{color:'white', opacity: '1'}}>Профиль</span>  
                 </Link>  
             </div>  
         </div>  
