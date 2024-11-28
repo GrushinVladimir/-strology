69aaf5eb685b60739533730829000033cfb276ea
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-28 12:25:32 +0300

    main

--- client/src/components/Body/main.jsx
+++ client/src/components/Body/main.jsx
@@ -1,196 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { load } from 'cheerio';
-import { useLocation, Link, useParams } from 'react-router-dom';
-
-const getApiKey = async () => {
-  try {
-    const response = await axios.get('https://strology.vercel.app/api/config-google');
-    return response.data.apiKeys;
-  } catch (error) {
-    console.error('Ошибка при получении ключа API:', error);
-    throw new Error('Не удалось получить ключ API');
-  }
-};
-const translateText = async (text) => {
-  try {
-    const apiKey = await getApiKey(); // Получаем ключ API
-    const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
-
-    const response = await axios.post(url, {
-      q: text,
-      target: 'ru',
-    });
-
-    return response.data.data.translations[0].translatedText;
-  } catch (error) {
-    console.error('Ошибка перевода текста:', error);
-    return text; // Возвращаем оригинальный текст в случае ошибки
-  }
-};
-
-const zodiacSigns = {
-  Водолей: 11,
-  Рыбы: 12,
-  Овен: 1,
-  Телец: 2,
-  Близнецы: 3,
-  Рак: 4,
-  Лев: 5,
-  Дева: 6,
-  Весы: 7,
-  Скорпион: 8,
-  Стрелец: 9,
-  Козерог: 10,
-};
-
-const zodiacImages = {  
-  Водолей: {  
-    src: 'img/zhak/vodoley2.png',  
-    width: '288px', // ширина  
-    height: 'auto' // высота  
-  },  
-  Рыбы: {  
-    src: 'img/zhak/riby2.png',  
-    width: '311px',  
-    height: 'auto'  
-  },  
-  Овен: {  
-    src: 'img/zhak/oven2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Телец: {  
-    src: 'img/zhak/telec2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Близнецы: {  
-    src: 'img/zhak/bliznecy2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Рак: {  
-    src: 'img/zhak/rak2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Лев: {  
-    src: 'img/zhak/lev2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Дева: {  
-    src: 'img/zhak/deva2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Весы: {  
-    src: 'img/zhak/vesy2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Скорпион: {  
-    src: 'img/zhak/scorpion2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Стрелец: {  
-    src: 'img/zhak/strelec2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-  Козерог: {  
-    src: 'img/zhak/kozerog2.png',  
-    width: '288px',  
-    height: 'auto'  
-  },  
-};  
-
-
-const getHoroscope = async (zodiacSign, period) => {  
-  try {  
-    const response = await axios.get(`/api/horoscope`, {  
-      params: { zodiacSign, period }, // Передаем параметры в запросе  
-    });  
-
-    return response.data.horoscope;  
-  } catch (error) {  
-    console.error('Ошибка при получении гороскопа:', error);  
-    return 'Не удалось получить гороскоп';  
-  }  
-};  
-
-const OneDate = (date) => {
-  const day = date.getDate().toString().padStart(2, '0');
-  return `${day}`;
-};
-
-const formatDate = (date) => {
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  return `${day}.${month}.${year}`;
-};
-
-const getWeekRange = () => {
-  const currentDate = new Date();
-  const firstDay = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 1));
-  const lastDay = new Date(currentDate.setDate(currentDate.getDate() + 6));
-  return `${OneDate(firstDay)} - ${formatDate(lastDay)}`;
-};
-
-const getMonthRange = () => {
-  const currentDate = new Date();
-  const month = currentDate.toLocaleString('default', { month: 'long' });
-  const year = currentDate.getFullYear();
-  const firstDay = new Date(year, currentDate.getMonth(), 1);
-  const lastDay = new Date(year, currentDate.getMonth() + 1, 0);
-  return `${OneDate(firstDay)} - ${formatDate(lastDay)}`;
-};
-
-const useTelegramId = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  return queryParams.get('telegramId'); // Здесь извлекаем telegramId из URL
-};
-
-const MainPage = ({ telegramId }) => { // Получаем telegramId через пропсы
-  const [zodiacSign, setZodiacSign] = useState(null);
-  const [horoscope, setHoroscope] = useState('');
-  const [showTabContent, setShowTabContent] = useState(false);
-  const [activeTab, setActiveTab] = useState('Сегодня');
-  const [currentDate, setCurrentDate] = useState('');
-  const [userData, setUserData] = useState(null);
-  const [showContent, setShowContent] = useState(false);  
+const MainPage = ({ telegramId }) => {  
+  const [zodiacSign, setZodiacSign] = useState(null);  
+  const [horoscope, setHoroscope] = useState('');  
+  const [activeTab, setActiveTab] = useState('Сегодня');  
+  const [currentDate, setCurrentDate] = useState('');  
+  const [userData, setUserData] = useState(null);  
   const [loading, setLoading] = useState(true);  
   const [error, setError] = useState(null);  
   const [tabLoading, setTabLoading] = useState(false);  
+  const [showTabContent, setShowTabContent] = useState(false);  
 
+  useEffect(() => {  
+    if (!telegramId) return;  
 
-  useEffect(() => {
-    if (!telegramId) return; // Если нет telegramId, выходим из useEffect
+    const fetchUserData = async () => {  
+      try {  
+        const response = await axios.get(`/api/users/${telegramId}`);  
+        if (response.data && response.data.user) {  
+          setUserData(response.data.user);  
+          setZodiacSign(response.data.user.zodiacSign);  
+        } else {  
+          console.error('Данные пользователя не найдены');  
+        }  
+      } catch (error) {  
+        console.error('Ошибка при получении данных пользователя:', error);  
+        setError('Ошибка при получении данных пользователя');  
+      } finally {  
+        setLoading(false); // Устанавливаем loading в false после завершения загрузки данных пользователя  
+      }  
+    };  
 
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get(`/api/users/${telegramId}`); // Запрос к серверу для получения данных
-        if (response.data && response.data.user) {
-          setUserData(response.data.user);
-          setZodiacSign(response.data.user.zodiacSign); // Устанавливаем знак зодиака
-        } else {
-          console.error('Данные пользователя не найдены');
-        }
-      } catch (error) {
-        console.error('Ошибка при получении данных пользователя:', error);
-      }
-    };
+    fetchUserData();  
+  }, [telegramId]);  
 
-    fetchUserData(); // Загружаем данные пользователя
-  }, [telegramId]); // Используем telegramId в зависимости
-  
   useEffect(() => {  
     if (!zodiacSign) return;  
-  
+
     const fetchAndTranslateHoroscope = async () => {  
       const periodKey = {  
         'Сегодня': 'today',  
@@ -198,7 +42,7 @@ const MainPage = ({ telegramId }) => { // Получаем telegramId через
         'Неделя': 'week',  
         'Месяц': 'month',  
       }[activeTab] || '';  
-  
+
       let formattedDate = '';  
       switch (activeTab) {  
         case 'Сегодня':  
@@ -218,7 +62,8 @@ const MainPage = ({ telegramId }) => { // Получаем telegramId через
       }  
       setCurrentDate(formattedDate);  
       setTabLoading(true); // Устанавливаем состояние загрузки в true для табов  
-      setShowContent(false); // Скрываем контент перед загрузкой  
+      setShowTabContent(false); // Скрываем контент перед загрузкой  
+
       try {  
         const horoscopeText = await getHoroscope(zodiacSign, periodKey);  
         if (horoscopeText) {  
@@ -231,107 +76,104 @@ const MainPage = ({ telegramId }) => { // Получаем telegramId через
         console.error('Ошибка при получении гороскопа:', error);  
         setHoroscope('Не удалось получить гороскоп');  
       } finally {  
-        setTabLoading(false); 
-        setTimeout(() => {  
-          setShowContent(true); // Показываем контент с задержкой  
-        }, 500); // Задержка в 500 мс  
+        setTabLoading(false); // Устанавливаем состояние загрузки в false после завершения  
+        setShowTabContent(true); // Показываем контент после загрузки  
       }  
     };  
-  
+
     fetchAndTranslateHoroscope();  
   }, [activeTab, zodiacSign]);  
 
-  if (loading) {
-    return (
-        <div className="loading-overlay">
-            <div className="loader"></div>
-        </div>
-    );
-}
-
-if (error) return <p>{error}</p>;
-
-if (tabLoading) {  
-  return (  
+  if (loading) {  
+    return (  
       <div className="loading-overlay">  
-          <div className="loader">Загрузка гороскопа...</div>  
+        <div className="loader">Загрузка данных пользователя...</div>  
       </div>  
-  );  
-} 
-  return (
-  <div className="main-page">
+    );  
+  }  
 
-    <h2 style={{ marginTop: '10vh' }}>Ваш знак</h2>
-    {zodiacSign ? (
-    <>
-      <h1>{zodiacSign}</h1>
-      <div className="zodiac-image">  
-  {zodiacImages && zodiacImages[zodiacSign] ? (  
-    <img  
-      src={zodiacImages[zodiacSign].src}  
-      alt={zodiacSign}  
-      style={{ maxWidth: zodiacImages[zodiacSign].width, height: zodiacImages[zodiacSign].height }}  
-    />  
-  ) : (  
-    <p>Изображение для знака зодиака не найдено</p>  
-  )}  
-</div> 
-    </>
-  ) : (
-    <p>Знак зодиака...</p>
-  )}
+  if (error) return <p>{error}</p>;  
 
-      <div className="tabs-and-content">
-        {showTabContent && activeTab && (
-          <div className="toggle-icon" onClick={() => setShowTabContent(!showTabContent)}>
-            <img src="free-icon-down-chevron-10728680.png" alt="" style={{ width: '35px' }} />
-          </div>
-        )}
+  if (tabLoading) {  
+    return (  
+      <div className="loading-overlay">  
+        <div className="loader">Загрузка гороскопа...</div>  
+      </div>  
+    );  
+  }  
 
-        <div className="tabs">
-          {['Сегодня', 'Завтра', 'Неделя', 'Месяц'].map((tab) => (
-            <button
-              key={tab}
-              className={activeTab === tab ? 'active' : ''}
-              onClick={() => {
-                setActiveTab(tab);
-                setShowTabContent(true);
-              }}
-            >
-              {tab}
-            </button>
-          ))}
-        </div>
+  return (  
+    <div className="main-page">  
+      <h2 style={{ marginTop: '10vh' }}>Ваш знак</h2>  
+      {zodiacSign ? (  
+        <>  
+          <h1>{zodiacSign}</h1>  
+          <div className="zodiac-image">  
+            {zodiacImages && zodiacImages[zodiacSign] ? (  
+              <img  
+                src={zodiacImages[zodiacSign].src}  
+                alt={zodiacSign}  
+                style={{ maxWidth: zodiacImages[zodiacSign].width, height: zodiacImages[zodiacSign].height }}  
+              />  
+            ) : (  
+              <p>Изображение для знака зодиака не найдено</p>  
+            )}  
+          </div>  
+        </>  
+      ) : (  
+        <p>Знак зодиака не найден...</p>  
+      )}  
+
+      <div className="tabs-and-content">  
+        {showTabContent && activeTab && (  
+          <div className="toggle-icon" onClick={() => setShowTabContent(!showTabContent)}>  
+            <img src="free-icon-down-chevron-10728680.png" alt="Toggle" style={{ width: '35px' }} />  
+          </div>  
+        )}  
+
+        <div className="tabs">  
+          {['Сегодня', 'Завтра', 'Неделя', 'Месяц'].map((tab) => (  
+            <button  
+              key={tab}  
+              className={activeTab === tab ? 'active' : ''}  
+              onClick={() => {  
+                setActiveTab(tab);  
+                setShowTabContent(true);  
+              }}  
+            >  
+              {tab}  
+            </button>  
+          ))}  
+        </div>  
 
         <div className="tab-content-container">  
-  {showTabContent && (  
-    <>  
-          <>  
-            <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>{currentDate}</p>  
-            <p style={{ textAlign: 'left', lineHeight: '1rem', fontSize: '14px', paddingBottom: '2rem' }}>{horoscope || 'Гороскоп пока недоступен'}</p>  
-          </>  
+          {showTabContent && (  
+            <>  
+              <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>{currentDate}</p>  
+              <p style={{ textAlign: 'left', lineHeight: '1rem', fontSize: '14px', paddingBottom: '2rem' }}>  
+                {horoscope || 'Гороскоп пока недоступен'}  
+              </p>  
+            </>  
+          )}  
+        </div>  
 
-    </>  
-  )}  
-</div>  
+        <div className="menu">  
+          <Link to="/main">  
+            <img src="img/menu/home.png" alt="Home" />  
+            <span style={{ color: 'white', opacity: '1' }}>Главная</span>  
+          </Link>  
+          <Link to="/chat">  
+            <img src="img/menu/chat.png" alt="Chat" />  
+            <span>Чат</span>  
+          </Link>  
+          <Link to={`/profile?telegramId=${telegramId}`}>  
+            <img src="img/menu/profile.png" style={{ width: '13px' }} alt="Profile" />  
+            <span>Профиль</span>  
+          </Link>  
+        </div>  
+      </div>  
+    </div>  
+  );  
+};  
 
-        <div className="menu">
-          <Link to="/main">
-            <img src="img/menu/home.png" />
-            <span style={{color:'white',opacity: '1'}}>Главная</span>
-          </Link>
-          <Link to="/chat">
-            <img src="img/menu/chat.png" />
-            <span>Чат</span>
-          </Link>
-          <Link to={`/profile?telegramId=${telegramId}`}>
-        <img src="img/menu/profile.png" style={{ width: '13px' }} />
-        <span>Профиль</span>
-      </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default MainPage;
+export default MainPage;  
