411bea3fb026f5c349a271ef9bdcdbb074208927
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-28 10:24:58 +0300

    info-znak

--- client/src/components/Body/ProfilePage.jsx
+++ client/src/components/Body/ProfilePage.jsx
@@ -195,8 +195,13 @@ const ProfilePage = ({ telegramId }) => {
                     {userData && (  
                         <div className="profile-desk">  
                             <h4 style={{fontWeight: '200'}}>О вашем знаке: <span style={{textTransform: 'uppercase'}}>{zodiacSign || 'Не найден'}</span></h4>  
-                            <p style={{position: 'relative'}}>
-                            {showFullDescription ? userData.zodiacDescription : truncateDescription(userData.zodiacDescription)}
+                            <div style={{ overflow: 'hidden', transition: 'max-height 0.5s ease', maxHeight: showFullDescription ? '500px' : '0' }}>
+                        <p style={{ margin: 0 }}>
+                            {userData.zodiacDescription}
+                        </p>
+                    </div>
+                    <p style={{ position: 'relative' }}>
+                        {showFullDescription ? userData.zodiacDescription : truncateDescription(userData.zodiacDescription)}
                         <span 
                             onClick={() => setShowFullDescription(!showFullDescription)} 
                             style={{ cursor: 'pointer', right: '15px', position: 'absolute', bottom: '0' }}
