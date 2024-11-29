82a25a10b8371e7834064eafff40e5b34c67fe1c
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-11-29 14:15:22 +0300

    fav

--- client/src/components/Body/main.jsx
+++ client/src/components/Body/main.jsx
@@ -278,7 +278,7 @@ if (error) return <p>{error}</p>;
       <div className="tabs-and-content">
         {showTabContent && activeTab && (
           <div className="toggle-icon" onClick={() => setShowTabContent(!showTabContent)}>
-            <img src="ard.png" alt="" style={{ width: '20px', background: '#4c3997', borderRadius: '50%',  padding: '5px'}} />
+            <img src="ard.png" alt="" style={{ width: '20px', background: '#4c3997', borderRadius: '50%',  padding: '5px', border: '.5px solid #e7dfdf52'}} />
           </div>
         )}
 
